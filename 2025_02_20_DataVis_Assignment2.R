dd1ab6a042c80d27944e743a36e86c6c648c4428
kingjad33 <jzk0179@auburn.edu>
2025-02-20 18:22:04 -0600

    Completed assignment

--- 2025_02_20_DataVis_Assignment2.R
+++ 2025_02_20_DataVis_Assignment2.R
@@ -10,21 +10,21 @@ mycodata$Treatment=as.factor(mycodata$Treatment)
 mycodata$Cultivar=as.factor(mycodata$Cultivar)
 str(mycodata)
 
-#Question 1
+#Question 1- create boxplot with needed characteristics
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 colorchoice= c("#56B4E9","#009E73")
-graph1= ggplot(mycodata, aes(x=Treatment, y=DON, fill=Cultivar)) +
-  geom_boxplot(position=position_dodge(0.5),outlier.color = "NA") +
-  xlab("") + ylab("DON (ppm)") +
-  geom_point(pch=21, alpha=0.6, position=position_jitterdodge(dodge.width=0.9)) +
-  scale_fill_manual(values= colorchoice)+
+graph1= ggplot(mycodata, aes(x=Treatment, y=DON, fill=Cultivar)) + #fill ensures whol box is same color
+  geom_boxplot(position=position_dodge(0.5),outlier.color = "NA") + #outlier NA ensures outlier points are not in the graph
+  xlab("") + ylab("DON (ppm)") + #axis labels
+  geom_point(pch=21, alpha=0.6, position=position_jitterdodge(dodge.width=0.9)) + #pch is shape type and alpha is point transparency
+  scale_fill_manual(values= colorchoice)+ #tell R which colors to use
   theme_classic()+
-  facet_wrap(~Cultivar)
+  facet_wrap(~Cultivar) #creates different graphs for each cultivar
 graph1
 
 #Question 2
-mycodata$Treatment2= factor(mycodata$Treatment, levels= c("NTC","Fg","Fg + 37","Fg + 40","Fg + 70"))
-graph2= ggplot(mycodata, aes(x=Treatment2, y=DON, fill=Cultivar)) +
+mycodata$Treatment2= factor(mycodata$Treatment, levels= c("NTC","Fg","Fg + 37","Fg + 40","Fg + 70")) #new column with correct order
+graph2= ggplot(mycodata, aes(x=Treatment2, y=DON, fill=Cultivar)) + #uses corrected order
   geom_boxplot(position=position_dodge(0.5),outlier.color = "NA") +
   xlab("") + ylab("DON (ppm)") +
   geom_point(pch=21, alpha=0.6, position=position_jitterdodge(dodge.width=0.9)) +
@@ -33,7 +33,7 @@ graph2= ggplot(mycodata, aes(x=Treatment2, y=DON, fill=Cultivar)) +
   facet_wrap(~Cultivar)
 graph2
 
-#Question 3
+#Question 3- change the y axis from DON 
 graph3= ggplot(mycodata, aes(x=Treatment2, y=X15ADON, fill=Cultivar)) +
   geom_boxplot(position=position_dodge(0.5),outlier.color = "NA") +
   xlab("") + ylab("15ADON") +
